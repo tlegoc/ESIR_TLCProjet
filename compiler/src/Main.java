8575f4a7ec2647f084316dfc43fcd222f1b2f136
Théo Le Goc <theo@legoc.net>
2024-01-02 17:47:32 +0100

    arguments

--- compiler/src/Main.java
+++ compiler/src/Main.java
@@ -13,17 +13,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("While Compiler cyberviking 2019");
 
-        if (args.length <= 0) {
+        if (args.length <= 1) {
             System.out.println("No input specified! Please input a file.");
             return;
         }
 
         String filename = args[0];
-        System.out.println("Compiling " + filename);
+        File f = new File(filename);
+        if (!f.exists())
+        {
+            System.out.println("Error: input doesn't exist.");
+        } else if (f.isDirectory())
+        {
+            System.out.println("Error: input is a directory");
+        }
+        String main = args[1];
+        System.out.println("Compiling " + filename + " with main " + main);
 
         WhileCompiler compiler = new WhileCompiler(filename);
 
-        boolean succes = compiler.compile("falsefunc", true, false);
+        boolean succes = compiler.compile(main, true, false);
 
         if (!succes) {
             System.out.println("Error, could not compile program");
