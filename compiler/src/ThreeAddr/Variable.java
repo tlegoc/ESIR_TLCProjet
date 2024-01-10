7a2596ec411930c6afb72255e0db651920293ecc
Théo Le Goc <theo@legoc.net>
2024-01-10 13:46:24 +0100

    Better arguments

--- compiler/src/ThreeAddr/Variable.java
+++ compiler/src/ThreeAddr/Variable.java
@@ -1,14 +1,14 @@
 package ThreeAddr;
 
-public class Symbol implements Argument {
+public class Variable  implements Argument {
     public String name = "";
 
-    public Symbol(String _name) {
+    public Variable(String _name) {
         char[] tmp = new char[2];
-        _name.toLowerCase().getChars(0, 1, tmp, 0);
+        _name.toUpperCase().getChars(0, 1, tmp, 0);
         _name.getChars(0, 1, tmp, 1);
         if (tmp[0] != tmp[1]) {
-            throw new RuntimeException("Error: symbol should start with a lower case letter.");
+            throw new RuntimeException("Error: variable should start with a upper case letter.");
         }
         name = _name;
     }
