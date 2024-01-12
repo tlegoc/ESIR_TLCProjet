f1726af48828cc4b6a8b81fa6e7a82a6a228abc6
Théo Le Goc <theo@legoc.net>
2024-01-12 18:24:12 +0100

    Better compile routine

--- lib/compile_windows.bat
+++ lib/compile_windows.bat
@@ -1,10 +1,17 @@
 @echo off
+set /p "VISUALDIR=Entrez le chemin d'installation de Visual Studio: "
+if not "%VISUALDIR%" == "" goto :continue
 set VISUALDIR=C:\Program Files\Microsoft Visual Studio\2022\Community
 
-REM -- mise en place environnement Visual
+:continue
 pushd "%VISUALDIR%\VC\Auxiliary\Build"
 call vcvarsall.bat x64
 popd
 
+rem Compiling objects
 cl /EHsc /c lib_while.cpp /I ./
-lib /OUT:while.lib lib_while.obj
+rem Actual library compilation
+lib /OUT:../compiler/while.lib lib_while.obj
+
+rem Copying lib_while.h
+copy "lib_while.h" "../compiler/lib_while.h"
